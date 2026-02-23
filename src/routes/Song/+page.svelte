c404e73c0133c55839e7b7f7c496bda5e7cf43cc
binsim <jonasspitzhirn@gmail.com>
2026-02-23 18:10:27 +0100

    show database content in web

---
+++ src/routes/Song/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+</script>
+
+{#each data.songs as song}
+	<div>
+		<h2>{song.title}</h2>
+		<p>{song.artist}</p>
+	</div>
+{/each}
