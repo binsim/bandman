b8f7f04d58d35fafe531246ec03ed021af162242
binsim <jonasspitzhirn@gmail.com>
2026-03-01 11:51:11 +0100

    add logout button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import favicon from '$lib/assets/favicon.svg';
+
+	async function logout() {
+		const response = await fetch('/', {
+			method: 'POST',
+			credentials: 'same-origin',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+
+		if (!response.ok) {
+			throw new Error('Failed to logout');
+		}
+
+		location.href = '/login';
+	}
 
 	let { children } = $props();
 </script>
@@ -9,10 +26,30 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<header></header>
+{#if page.url.pathname !== '/login' && page.url.pathname !== '/register'}
+	<header>
+		<nav>
+			<li><a href="/">Home</a></li>
+		</nav>
+
+		<div class="logout">
+			<button onclick={logout}>Logout</button>
+		</div>
+	</header>
+{/if}
 
 <main>
 	{@render children()}
 </main>
 
-<footer></footer>
+{#if page.url.pathname !== '/login' && page.url.pathname !== '/register'}
+	<footer></footer>
+{/if}
+
+<style lang="scss">
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+</style>
